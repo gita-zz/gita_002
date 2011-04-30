7f1c0186db28c4a5eb446b6f3ad4256580b12e3d
Albert Leśnikowski <pan@albercik.net>
2011-05-01 00:06:08 +0200

    pierwszy commit

---
+++ src/pakiecik/Starter.java
@@ -0,0 +1,14 @@
+package pakiecik;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        new Starter();
+    }
+    
+    public Starter() {
+        System.out.println(" -> Startujemy");
+    }
+    
+    
+}
